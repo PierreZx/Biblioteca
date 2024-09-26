a11881ae968774d6d790fd45c83c52b0c3f2171a
Hugo Pierre <hugo.pierre12255@gmail.com>
2024-09-26 17:22:02 -0300

    Muita coisa a ser trabalhada ainda

--- aguarte.py
+++ aguarte.py
@@ -376,61 +376,178 @@ def main(page: ft.Page):
         popup.open = True
         page.update()
 
+    def show_popup_livro_cadastrado():
+        popup = ft.AlertDialog(title="Cadastro de Livro", content=ft.Text("Livro cadastrado com sucesso!"))
+        page.overlay.append(popup)
+
+    def executar_script_sql_r(titulo, autor, genero, idioma, localizacao):
+        tit = titulo.value
+        aut = autor.value
+        gen = genero.value
+        idi = idioma.value
+        loc = localizacao.value
+
+        if verifica_livro_existente(tit, aut):
+            show_popup_livro_existente()
+            return
+
+        livro = {
+            "titulo": tit,
+            "Autor": aut,
+            "Genero": gen,
+            "Idioma": idi,
+            "loc": loc,
+            "status": "Disponível"  # Definindo o status como "Disponível"
+        }
+
+        cursor = None
+        try:
+            conn = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="acesso123",
+                database="biblioteca",
+                port="3306"
+            )
+            cursor = conn.cursor()
+            cursor.execute(
+                "INSERT INTO books (titulo, autor, genero, idioma, localizacao, status) VALUES (%s, %s, %s, %s, %s, %s)",
+                (livro["titulo"], livro["Autor"], livro["Genero"], livro["Idioma"], livro["loc"], livro["status"])
+            )
+            conn.commit()
+            print("Registro inserido com sucesso na tabela 'books'!")
+            show_popup_livro_cadastrado()
+        except Exception as e:
+            print(f"Erro ao conectar ou executar o script SQL: {e}")
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                conn.close()
+                print("Conexão com o banco de dados fechada!")
+
+    def remover_livro(titulo):
+        cursor = None
+        try:
+            conn = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="acesso123",
+                database="biblioteca",
+                port="3306"
+            )
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM books WHERE titulo = %s", (titulo,))
+            conn.commit()
+            if cursor.rowcount > 0:
+                print("Livro removido com sucesso!")
+            else:
+                print("Nenhum livro encontrado com o título fornecido.")
+        except Exception as e:
+            print(f"Erro ao conectar ou executar o script SQL: {e}")
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                conn.close()
+                print("Conexão com o banco de dados fechada!")
+
     def page_normal():
-        titulo = ft.TextField(label="Título")
-        autor = ft.TextField(label="Autor")
-        genero = ft.TextField(label="Gênero")
-        idioma = ft.TextField(label="Idioma")
-        localizacao = ft.TextField(label="Localização")
+        page.controls.clear()
+
+        titulo_principal = ft.Text(
+            "Biblioteca Aservo",
+            color=ft.colors.WHITE,
+            size=30,
+            weight=ft.FontWeight.BOLD,
+            text_align=ft.TextAlign.CENTER
+        )
+
+        titulo_principal2 = ft.Text(
+            "Adminastrador",
+            color=ft.colors.WHITE,
+            size=10,
+            weight=ft.FontWeight.BOLD,
+            text_align=ft.TextAlign.CENTER
+        )
+
+        imagem = ft.Image(
+            src="C:/Users/1009061/Downloads/abra-o-livro.png",
+            width=80,
+            height=80,
+            fit=ft.ImageFit.CONTAIN,
+        )
+
+        titulo_com_imagem = ft.Row(
+            controls=[imagem, titulo_principal],
+            alignment=ft.MainAxisAlignment.CENTER,  # Centraliza o título e a imagem
+        )
+
+        # Campos de entrada para cadastrar um novo livro
+        titulo = ft.TextField(label="Título", width=250)
+        autor = ft.TextField(label="Autor", width=250)
+        genero = ft.TextField(label="Gênero", width=250)
+        idioma = ft.TextField(label="Idioma", width=250)
+        localizacao = ft.TextField(label="Localização", width=250)
         button3 = ft.TextButton(text="Cadastrar livro", on_click=lambda e: executar_script_sql_r(titulo, autor, genero, idioma, localizacao))
 
-        frame_livro = ft.Container(
-            content=ft.Column(
-                controls=[
-                    titulo,
-                    autor,
-                    genero,
-                    idioma,
-                    localizacao,
-                    button3
-                ]
-            ),
+        cadastro_coluna = ft.Column(
+            controls=[
+                ft.Text("Cadastro de Livro", color=ft.colors.WHITE, size=15, weight=ft.FontWeight.BOLD),
+                titulo,
+                autor,
+                genero,
+                idioma,
+                localizacao,
+                button3
+            ],
+            alignment=ft.MainAxisAlignment.START,
+            horizontal_alignment=ft.CrossAxisAlignment.START
+        )
+
+        # Adicionando a coluna de cadastro em um container para facilitar a posição
+        cadastro_container = ft.Container(
+            content=cadastro_coluna,
+            padding=10,
+            width=300,  # Define a largura desejada para o container de cadastro
             bgcolor="#696969",
-            width=400,
-            height=400,
+            border_radius=10
+        )
+
+        frame_livro = ft.Container(
+            content=ft.Row(
+                controls=[
+                    cadastro_container,
+                ],
+                alignment=ft.MainAxisAlignment.CENTER
+            ),
+            bgcolor="#505050",
+            width=2000,
+            height=600,
             border_radius=10,
-            alignment=ft.alignment.center,
             padding=20
         )
 
-        page.controls.clear()
-        page.add(
-            ft.Container(
-                content=frame_livro,
-                alignment=ft.alignment.center
-            )
+        # Adicionando tudo ao container principal
+        main_container = ft.Column(
+            controls=[
+                titulo_com_imagem,
+                frame_livro
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.START
         )
+
+        page.add(main_container)
         page.update()
 
-        def executar_script_sql_r(titulo, autor, genero, idioma, localizacao):
-            tit = titulo.value
-            aut = autor.value
-            gen = genero.value
-            idi = idioma.value
-            loc = localizacao.value
-            if verifica_livro_existente(tit, aut):
-                show_popup_livro_existente()
-                return
-
-            livro = {
-                "titulo": tit,
-                "Autor": aut,
-                "Genero": gen,
-                "Idioma": idi,
-                "loc": loc
-            }
-
+    def Leitor_page():
+        def buscar_livro():
+            titulo_value = titulo.value
+            autor_value = autor.value
             cursor = None
+            resultados = []
+
             try:
                 conn = mysql.connector.connect(
                     host="localhost",
@@ -440,12 +557,32 @@ def main(page: ft.Page):
                     port="3306"
                 )
                 cursor = conn.cursor()
-                cursor.execute(
-                    "INSERT INTO books (titulo, autor, genero, idioma, localizacao) VALUES (%s, %s, %s, %s, %s)",
-                    (livro["titulo"], livro["Autor"], livro["Genero"], livro["Idioma"], livro["loc"])
-                )
-                conn.commit()
-                print("Registro inserido com sucesso na tabela 'books'!")
+                cursor.execute("SELECT * FROM books WHERE titulo = %s AND autor = %s", (titulo_value, autor_value))
+                resultados = cursor.fetchall()
+
+                if not resultados:
+                    show_popup_livro_nao_encontrado()
+                    Livros_data_table.rows = []
+                    Livros_data_table.update()
+                else:
+                    rows = []
+                    for livro in resultados:
+                        rows.append(
+                            ft.DataRow(
+                                cells=[
+                                    ft.DataCell(ft.Text(livro[0])),  # ID do livro
+                                    ft.DataCell(ft.Text(livro[1])),  # Título
+                                    ft.DataCell(ft.Text(livro[2])),  # Autor
+                                    ft.DataCell(ft.Text(livro[3])),  # Gênero
+                                    ft.DataCell(ft.Text(livro[4])),  # Idioma
+                                    ft.DataCell(ft.Text(livro[5])),  # Localização
+                                    ft.DataCell(ft.Text(livro[6])),  # Status
+                                ]
+                            )
+                        )
+                    Livros_data_table.rows = rows
+                    Livros_data_table.update()
+
             except Exception as e:
                 print(f"Erro ao conectar ou executar o script SQL: {e}")
             finally:
@@ -455,53 +592,102 @@ def main(page: ft.Page):
                     conn.close()
                     print("Conexão com o banco de dados fechada!")
 
-        print(f"Livro: {titulo.value}, Autor: {autor.value}, Gênero: {genero.value}, Idioma: {idioma.value}, Localização: {localizacao.value}")
-    def Leitor_page():
-
         page.controls.clear()
 
-        titulo = ft.TextField(label="Título")
-        autor = ft.TextField(label="Autor")
-        button3 = ft.TextButton(text="Buscar Livro", on_click=lambda e: buscar_livro(titulo.value, autor.value))
+        titulo_principal = ft.Text(
+            "Biblioteca Aservo",
+            color=ft.colors.WHITE,
+            size=30,
+            weight=ft.FontWeight.BOLD,
+            text_align=ft.TextAlign.CENTER
+        )
 
-        frame_livro = ft.Container(
+        imagem = ft.Image(
+            src="C:/Users/1009061/Downloads/abra-o-livro.png",
+            width=80,
+            height=80,
+            fit=ft.ImageFit.CONTAIN,
+        )
+
+        titulo_com_imagem = ft.Row(
+            controls=[imagem, titulo_principal],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+
+        titulo = ft.TextField(label="Título", width=250)
+        autor = ft.TextField(label="Autor", width=250)
+        button3 = ft.TextButton(text="Buscar Livro", on_click=lambda e: buscar_livro())
+
+        pesquisa_coluna = ft.Column(
+            controls=[
+                ft.Text("Busque seu livro", color=ft.colors.WHITE, size=15, weight=ft.FontWeight.BOLD),
+                titulo,
+                autor,
+                button3
+            ],
+            alignment=ft.MainAxisAlignment.START,
+            horizontal_alignment=ft.CrossAxisAlignment.START
+        )
+
+        Livros_data_table = ft.DataTable(
+            columns=[
+                ft.DataColumn(ft.Text("Título")),
+                ft.DataColumn(ft.Text("Autor")),
+                ft.DataColumn(ft.Text("Gênero")),
+                ft.DataColumn(ft.Text("Idioma")),
+                ft.DataColumn(ft.Text("Localização")),
+                ft.DataColumn(ft.Text("Status")),
+            ],
+            rows=[]
+        )
+
+        resultados_container = ft.Container(
             content=ft.Column(
                 controls=[
-                    titulo,
-                    autor,
-                    button3
+                    Livros_data_table
                 ]
             ),
+            padding=10,
+            width=300,
             bgcolor="#696969",
-            width=400,
-            height=400,
+            border_radius=10
+        )
+
+        pesquisa_container = ft.Container(
+            content=pesquisa_coluna,
+            padding=10,
+            width=300,
+            bgcolor="#696969",
+            border_radius=10
+        )
+
+        frame_livro = ft.Container(
+            content=ft.Row(
+                controls=[
+                    pesquisa_container,
+                    resultados_container
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+                spacing=20
+            ),
+            bgcolor="#505050",
+            width=700,
+            height=600,
             border_radius=10,
-            alignment=ft.alignment.center,
             padding=20
         )
 
-        frame_container = ft.Container(
-        content=frame_livro,
-        alignment=ft.alignment.center,
-        padding=20)
-
         main_container = ft.Column(
-        controls=[
-            frame_container
-        ],
-        alignment=ft.MainAxisAlignment.START,
-    )
+            controls=[
+                titulo_com_imagem,
+                frame_livro
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.START
+        )
 
         page.add(main_container)
         page.update()
-
-    def buscar_livro(titulo, autor):
-
-        if not verifica_livro_existente(titulo, autor):
-            show_popup_livro_nao_encontrado()
-        else:
-
-            pass
 
     def show_popup_livro_nao_encontrado():
         popup = ft.AlertDialog(
@@ -513,9 +699,10 @@ def main(page: ft.Page):
 
         def fechar_popup():
             popup.open = False
+            page.overlay.append(popup)
             page.update()
 
-        page.dialog = popup
+        page.overlay.append(popup)
         popup.open = True
         page.update()
 
